5c2b410dc86935eda745b0565b6921e9882f6796
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2019-09-17 17:49:18 +0100

    MU Moodle Enhancer 2019-2020

---
+++ mudle-enhancer.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Mudle Enhancer 
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Enhancements for Maynooth University's 2019-2020 Moodle Dashboard
+// @author       https://jeezeh.github.io
+// @match        https://2020.moodle.maynoothuniversity.ie/*
+// @grant        none
+// ==/UserScript== 
+
+(function () {
+    const courseListIds = ['#block-mycourselist-past', '#block-mycourselist-current', '#block-mycourselist-future'];
+    const courseLists = document.querySelectorAll(courseListIds.map(element => `${element} .course_title a[title]`).join(', '));
+    for (const course of courseLists) {
+        course.innerText = trimModule(course.title);
+    }
+
+    function trimModule(title) {
+        const cutPoint = title.indexOf('(2019');
+
+        return title.substring(0, cutPoint);
+    }
+
+})();
