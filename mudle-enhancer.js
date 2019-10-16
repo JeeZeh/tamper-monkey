5edb4ac525278ecff7ae70ebf0dbdc2a39890b9b
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2019-10-16 11:40:04 +0100

    If no cutpoint is found, don't cut.

--- mudle-enhancer.js
+++ mudle-enhancer.js
@@ -1,18 +1,15 @@
 // ==UserScript==
-// @name         Mudle Enhancer 
-// @namespace    http://tampermonkey.net/
+// @name         Mudle Enhancer
+// @namespace    https://github.com/JeeZeh/tamper-monkey/blob/master/mudle-enhancer.js
 // @version      1.0
 // @description  Enhancements for Maynooth University's 2019-2020 Moodle Dashboard
 // @author       https://jeezeh.github.io
 // @match        https://2020.moodle.maynoothuniversity.ie/*
 // @grant        none
-// ==/UserScript== 
+// ==/UserScript==
 
 cleanModuleNames();
 
-/**
- * Changes the ugly module code titles to their full module name, stripping the (YEAR:SEMESTER) at the end.
- */
 function cleanModuleNames() {
     const courseListIds = ['#block-mycourselist-past', '#block-mycourselist-current', '#block-mycourselist-future'];
     const courseLists = document.querySelectorAll(courseListIds.map(element => `${element} .course_title a[title]`).join(', '));
@@ -24,5 +21,9 @@ function cleanModuleNames() {
 function trimModule(title) {
     const cutPoint = title.indexOf('(2019');
 
-    return title.substring(0, cutPoint);
+    if (cutPoint > 0) {
+        return title.substring(0, cutPoint > 0 ? cutPoint : null);
+    }
+
+    return title;
 }
