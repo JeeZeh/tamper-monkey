3b638e3bce438baeb5d51060d6ebef56535a0b70
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-03-27 13:27:20 +0000

    Match fix

--- mule-paster.user.js
+++ mule-paster.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Mule Paster
 // @namespace    https://github.com/JeeZeh/tamper-monkey/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Enhancement for MULE environment allowing pasting from any source into workbook. Useful if working offline and moving your code to MULE afterwards. NOTE: You must paste, undo, and paste again if you have new clipboard content.
 // @author       https://jeezeh.github.io
-// @match        https://mule.cs.nuim.ie/*
+// @match        https://mule.cs.nuim.ie*
 // @include      https://*.githubusercontent.com/*
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
