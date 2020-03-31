03db5cc91a4e5c673544caad89afade1bd5f30ed
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-03-31 04:06:42 +0100

    Support all TLDs and run before content is loaded to avoid wasting time

--- amazon-smile-redirect.user.js
+++ amazon-smile-redirect.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Amazon Smile Auto Redirect
 // @namespace    https://github.com/JeeZeh/tamper-monkey
-// @version      1.0.1
+// @version      1.1.0
 // @description  If you always forget to use smile.amazon.* then this is for you! Redirects automatically if you every navigate to www.amazon.*. There may be bugs, haven't found any yet.
 // @author       https://jeezeh.github.io
-// @match        https://www.amazon.*
+// @include      https://www.amazon.tld/*
 // @include      https://*.githubusercontent.com/*
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
 // @connect      githubusercontent.com
 // @connect      raw.githubusercontent.com
+// @run-at       document-start
 // @updateURL    https://raw.githubusercontent.com/JeeZeh/tamper-monkey/master/amazon-smile-redirect.user.js
 // @downloadURL  https://raw.githubusercontent.com/JeeZeh/tamper-monkey/master/amazon-smile-redirect.user.js
 // @homepageURL  https://github.com/JeeZeh/tamper-monkey
